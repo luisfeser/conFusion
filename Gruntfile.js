bf959cda769cf0bf270f8988a5a5c7be3c9477fe
Felipe Retamar <felipe.retamar@gmail.com>
2016-01-30 10:38:51 +0100

    conFusion before third asignment

---
+++ Gruntfile.js
@@ -0,0 +1,180 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+	// Time how long task take. Can help when optimizing build times
+	require('time-grunt')(grunt);
+
+	// Automatically load required Grunt task
+	require('jit-grunt')(grunt, {
+		useminPrepare: 'grunt-usemin'
+	});
+
+	// Define the configuration for all the task
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		// Make sure code styles are up to par and there are no obviuos mistakes
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc',
+				reporter: require('jshint-stylish')
+			},
+			all: {
+				src: [
+					'Gruntfile.js',
+					'app/scripts/{,*/}*.js'
+				]
+			} 
+		},
+		useminPrepare: {
+			html: 'app/menu.html',
+			options: {
+				dest: 'dist'
+			}
+		},
+
+		//concat
+		concat: {
+			options: {
+				separator: ';'
+			},
+			// dist configuration is provided by useminPrepare
+			dist: {}
+		},
+
+		// Uglify
+		uglify: {
+			// dist configuration is provided by useminPrepare
+			dist: {}
+		},
+		cssmin: {
+			dist: {}
+		},
+		// Filerev
+		filerev: {
+			options: {
+				encoding: 'utf8',
+				algorithm: 'md5',
+				length: 20
+			},
+			release: {
+				// filerev:release hashes(md5) all assets (images, js and css )
+				// in dist directory
+				files: [{
+					src: [
+						'dist/styles/*.css',
+						'dist/scripts/*.js'
+					]
+				}]
+			}
+		},
+		//Usemin
+		//repaleces all assets with their revved version in html and css files.
+		// options.assetDirs contains the directories for finding the assets
+		// according to thir relative paths
+		usemin: {
+			html: ['dist/*.html'],
+			css: ['dist/styles/*.css'],
+			options: {
+				assetsDirs: ['dist', 'dist/styles']
+			}
+		},
+		copy: {
+			dist: {
+				cwd: 'app',
+				src: ['**','!styles/**/*.css','!scripts/**/*js'],
+				dest: 'dist',
+				expand: true
+			},
+			fonts: {
+				files:[
+					{
+						//for bootstrap fonts
+						expand: true,
+						dot: true,
+						cwd: 'bower_components/bootstrap/dist',
+						src: ['fonts/*.*'],
+						dest: 'dist'
+					},
+					{
+						//for font-awesome
+						expand: true,
+						dot: true,
+						cwd: 'bower_components/font-awesome',
+						src: ['fonts/*.*'],
+						dest: 'dist'
+					}
+				]
+			}
+		},
+		watch: {
+			copy: {
+				files: ['app/**', '!app/**/*.css', '!app/**/*.js'],
+				tasks: ['build']
+			},
+			scripts: {
+				files: ['app/scripts/app.js'],
+				tasks: ['build']
+			},
+			styles: {
+				files: ['app/styles/mystyles.css'],
+				tasks: ['build']
+			},
+			livereload: {
+				options: {
+					livereload: '<%= connect.options.livereload %>'
+				},
+				files: [
+					'app/{,*/}*.html',
+					'tmp/styles/{,*/}*.css',
+					'app/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+				]
+			}
+		},
+		connect: {
+			options: {
+				port: 9000,
+				//change this to '0.0.0.0' to accesss the server form outside
+				hostname: 'localhost',
+				livereload: 35729
+			},
+			dist: {
+				options: {
+					open: true,
+					base: {
+						path: 'dist',
+						options: {
+							index: 'menu.html',
+							maxAge: 300000
+						}
+					}
+				}
+			}
+		},
+		clean: {
+			build: {
+				src: ['dist/']
+			}
+		}
+	});
+
+
+	grunt.registerTask('build', [
+		'clean',
+		'jshint',
+		'useminPrepare',
+		'concat',
+		'cssmin',
+		'uglify',
+		'copy',
+		'filerev',
+		'usemin'
+	]);
+
+	grunt.registerTask('serve',['build','connect:dist','watch']);
+
+	grunt.registerTask('default',['build']);
+
+};
